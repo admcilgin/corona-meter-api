43ee6f5383c643b1a7baa317f6d984b0129cd6b5
acilgin <adem.cilgin@orioninc.com>
2022-03-08 17:35:07 +0300

    supabase integrated

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,13 +1,13 @@
 import got from "got";
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+import {supabase} from "../../utils/supabaseClient";
 
 export default async function handler(req, res) {
-
     let cases
     let deaths
     let recovered
-
+    let result
 
     await  got("https://www.worldometers.info/coronavirus/")
         .then(res =>new JSDOM(res.body))
@@ -17,6 +17,13 @@ export default async function handler(req, res) {
             deaths = counter[1].querySelector("span").textContent
             recovered = counter[2].querySelector("span").textContent
         })
+    // perform db insert
+    result = { cases:cases,deaths:deaths,recovered:recovered}
+    const {data,err} = await supabase
+        .from("corona_table")
+        .insert({data: result})
+
+    err ? console.log(err) : console.log(data);
 
     res.status(200).json({ cases:cases,deaths:deaths,recovered:recovered})
 
